0648b0c27b7aeb9fe29a6ef1c2c41f3fb54701d4
Solyman <solyman15x@gmail.com>
2023-12-03 11:55:01 +0100

    Day1-start

--- year-2023/src/main/java/nl/solyman/day1/AlgorithmDay1.java
+++ year-2023/src/main/java/nl/solyman/day1/AlgorithmDay1.java
@@ -2,4 +2,22 @@ package nl.solyman.day1;
 
 public class AlgorithmDay1 {
 
+    public int execute(String input) {
+
+        int total = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (Character.isDigit(currentChar)) {
+
+                int digit = Character.getNumericValue(currentChar);
+
+
+                System.out.println(digit);
+
+            }
+        }
+        return total;
+    }
 }
--- year-2023/src/test/java/nl/solyman/day1/AlgorithmDay1Test.java
+++ year-2023/src/test/java/nl/solyman/day1/AlgorithmDay1Test.java
@@ -1,7 +1,39 @@
 package nl.solyman.day1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AlgorithmDay1Test {
+
+    @Test
+    void part1_example() {
+        File file = new File(getClass().getClassLoader().getResource("day1/example.txt").getPath());
+        String input = readFile(file);
+
+        int output = new AlgorithmDay1().execute(input);
+
+        assertEquals(0, output);
+
+
+    }
+
+    @Test
+    void part1() {
+
+    }
+
+    public String readFile(File file) {
+        try {
+            return Files.readString(file.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
